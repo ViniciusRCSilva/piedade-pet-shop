ebdd52533a9736f2cfd0e2fd763bb73badf4eacc
Vinicius <viniciusflexa@gmail.com>
2024-12-12 11:36:59 -0300

    feat: update the product loading page

--- app/product/[id]/loading.tsx
+++ app/product/[id]/loading.tsx
@@ -1,5 +1,4 @@
 import { Skeleton } from "@/app/_components/ui/skeleton";
-import { Button } from "@/app/_components/ui/button";
 import Cart from "@/app/_components/cart";
 
 export default function ProductLoading() {
@@ -10,10 +9,17 @@ export default function ProductLoading() {
                     <Cart />
                 </div>
 
-                <Button variant="link" className="inline-flex items-center gap-2 mb-6 opacity-50">
-                    <Skeleton className="h-5 w-5" />
-                    <Skeleton className="h-4 w-24" />
-                </Button>
+                <div className="flex flex-col px-6 my-10 gap-8 lg:px-20">
+                    <div className="flex items-center gap-2">
+                        <Skeleton className="h-6 w-16" />
+                        <Skeleton className="h-4 w-4" />
+                        <Skeleton className="h-6 w-20" />
+                        <Skeleton className="h-4 w-4" />
+                        <Skeleton className="h-6 w-32" />
+                    </div>
+
+                    <div className="w-full h-[1px] bg-purple/20" />
+                </div>
 
                 <section className="flex w-full h-full justify-center mb-10">
                     <div className="container">
@@ -44,18 +50,25 @@ export default function ProductLoading() {
                     </div>
                 </section>
 
-                <div className="flex w-full justify-center mb-10">
-                    <div className="w-[90%] h-[1px] bg-purple/20" />
-                </div>
-
-                <section className="flex flex-col w-full h-full px-10 mb-10">
-                    <Skeleton className="h-6 w-32 mb-6" />
-                    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4">
-                        {[...Array(4)].map((_, i) => (
-                            <Skeleton key={i} className="h-[400px] rounded-lg" />
-                        ))}
+                <section className="flex flex-col w-full h-full gap-8 px-20 mb-10">
+                    <div className="w-full h-[1px] bg-purple/20" />
+                    <div className="flex flex-col">
+                        <Skeleton className="h-6 w-32 mb-6" />
+                        <div className="w-full px-20">
+                            <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4">
+                                {[1, 2, 3, 4].map((item) => (
+                                    <div key={item} className="p-1">
+                                        <Skeleton className="w-full h-[280px] rounded-lg" />
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
                     </div>
                 </section>
+
+                <footer className="flex h-[4vh] items-center justify-center bg-purple-foreground text-sm text-white lg:text-base">
+                    <p> 2024 Piedade PetShop. Todos os direitos reservados.</p>
+                </footer>
             </main>
         </>
     );
