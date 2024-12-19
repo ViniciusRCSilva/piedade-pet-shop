cdee7deaa8af9ba3ce835e05d65564fdfa4fd279
Vinicius <viniciusflexa@gmail.com>
2024-12-18 21:03:47 -0300

    feat: new content for the about us

--- app/(home)/_components/about-us-section/index.tsx
+++ app/(home)/_components/about-us-section/index.tsx
@@ -6,27 +6,47 @@ const AboutUsSection = () => {
         <div className="w-[80%] flex flex-col items-center lg:items-start">
             <h1 className="text-2xl font-bold text-purple mb-4">Piedade PetShop</h1>
             <p className="mb-2 text-muted-foreground text-center text-sm lg:text-base lg:text-left">
-                A relação entre um tutor e um animal de estimação é transformadora, cheia de conexão emocional. Para tornar esse laço ainda mais especial, o Piedade PetShop oferece produtos dos principais fabricantes do mercado.
-                Com toda essa variedade, os tutores podem encontrar o que precisam no dia a dia sem sair de casa. A loja do Piedade PetShop tem várias opções de produtos, todos organizados por categorias!
+                A relação entre tutores e seus animais de estimação é verdadeiramente transformadora,
+                repleta de conexão emocional. Para tornar esse vínculo ainda mais especial,
+                o Piedade PetShop oferece produtos dos principais fabricantes do mercado.
+                Com uma vasta variedade de opções, os tutores podem encontrar tudo o que precisam para o
+                dia a dia dos seus pets, sem sair de casa. Nossa loja possui diversas categorias de produtos,
+                todas organizadas para facilitar sua busca!
             </p>
             <Accordion type="single" collapsible className="w-full flex flex-col gap-4">
                 <Content
                     value="pets-24h"
                     title="Tudo que os pets precisam a qualquer hora do dia"
-                    description="Um dos principais lemas do Piedade PetShop é a conveniência. Por isso, nossa loja online está disponível 24h por dia. Sempre que precisarem, os tutores podem contar com esse auxílio para cuidar da saúde e do conforto dos amigos com patas. 
-                    Os produtos e serviços do Piedade PetShop refletem nossa paixão pelos animais de estimação. Aqui, eles recebem carinho e respeito. O catálogo tem itens de alta qualidade, à altura do amor que os pets merecem! Tudo isso de maneira organizada e acessível, facilitando a vida dos tutores."
+                    description="A conveniência é um dos principais lemas do Piedade PetShop. 
+                    Nossa loja online está disponível 24 horas por dia, permitindo que os tutores 
+                    cuidem da saúde e do conforto dos seus amigos de quatro patas a qualquer 
+                    momento. Os produtos e serviços do Piedade PetShop refletem nossa paixão 
+                    pelos animais de estimação, oferecendo carinho e respeito. 
+                    Nosso catálogo inclui itens de alta qualidade, à altura do amor que os 
+                    pets merecem, tudo de maneira organizada e acessível para facilitar a vida 
+                    dos tutores."
                 />
                 <Content
                     value="dogs"
                     title="Cachorros: alimentos, medicamentos e outros itens"
-                    description="Os cães são verdadeiros companheiros! Eles precisam de alguns cuidados básicos, como a alimentação. Pensando nisso, há uma grande variedade de rações e petiscos para a dieta, incluindo opções de cuidado específico para determinadas raças e condições de saúde. 
-                    Além de uma nutrição adequada, os cachorros precisam de medicamentos para tratar e prevenir doenças. Vale lembrar que todo remédio deve ser prescrito por um médico-veterinário. Isso é importante para preservar a qualidade de vida dos animais de estimação."
+                    description="Os cães são verdadeiros companheiros e merecem cuidados especiais. 
+                    Pensando nisso, o Piedade PetShop oferece uma grande variedade de rações e 
+                    petiscos para atender às necessidades específicas de cada raça e condição de 
+                    saúde. Além de uma nutrição adequada, é essencial que os cachorros recebam 
+                    medicamentos para tratar e prevenir doenças, sempre com a prescrição de um 
+                    médico-veterinário. Isso é fundamental para garantir a qualidade de vida dos 
+                    nossos amados pets."
                 />
                 <Content
                     value="cats"
                     title="Gatos: areias, brinquedos e outros produtos"
-                    description="Além de muitas comidinhas e remédios para felinos, o Piedade PetShop oferece diversos outros produtos para gatos! Entre eles, há opções de areias e caixinhas higiênicas, que proporcionam mais conforto para o animal fazer as necessidades básicas. 
-                    Você também pode deixar o dia a dia dos gatinhos mais feliz com brinquedos e outros acessórios, como os arranhadores. Esses itens estimulam a mente dos mascotes, promovendo a qualidade de vida e a longevidade dos pets."
+                    description="Além de uma ampla variedade de comidinhas e remédios para felinos,
+                    o Piedade PetShop oferece diversos outros produtos para gatos! Entre eles, 
+                    você encontrará opções de areias e caixinhas higiênicas, proporcionando
+                    mais conforto para o seu pet fazer suas necessidades básicas. 
+                    Também temos brinquedos e acessórios, como arranhadores, que deixam o 
+                    dia a dia dos gatinhos mais feliz e estimulam a mente dos mascotes, 
+                    promovendo a qualidade de vida e a longevidade dos pets."
                 />
             </Accordion>
         </div>
